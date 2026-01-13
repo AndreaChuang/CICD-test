bbc254f574c40161079b1edcbd3bf2aecbbe34d4
Andrea.Chuang <andrea.chuang@saa-symtek.com>
2026-01-13 14:27:41 +0800

    fix: 解決測試環境缺少 document 的問題

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vitest/config";
 
-// https://vite.dev/config/
+// 建議將 Vite 與 Vitest 的設定合併
 export default defineConfig({
   plugins: [vue()],
   test: {
